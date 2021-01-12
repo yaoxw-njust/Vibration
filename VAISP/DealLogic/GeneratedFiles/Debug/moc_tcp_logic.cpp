f7208d2e0d2f9856f46f2a78f28a804b9e54e7e6
yaoxw-njust <yaoxw13@163.com>
2021-01-12 16:07:25 +0800

    太和代码提交

---
+++ VAISP/DealLogic/GeneratedFiles/Debug/moc_tcp_logic.cpp
@@ -0,0 +1,431 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'tcp_logic.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../tcp_logic.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'tcp_logic.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_TcpServer_t {
+    QByteArrayData data[8];
+    char stringdata0[76];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TcpServer_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TcpServer_t qt_meta_stringdata_TcpServer = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "TcpServer"
+QT_MOC_LITERAL(1, 10, 14), // "pushSocketData"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 5), // "bytes"
+QT_MOC_LITERAL(4, 32, 12), // "sendStateMsg"
+QT_MOC_LITERAL(5, 45, 3), // "msg"
+QT_MOC_LITERAL(6, 49, 4), // "flag"
+QT_MOC_LITERAL(7, 54, 21) // "onServerNewConnection"
+
+    },
+    "TcpServer\0pushSocketData\0\0bytes\0"
+    "sendStateMsg\0msg\0flag\0onServerNewConnection"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TcpServer[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   29,    2, 0x06 /* Public */,
+       4,    2,   32,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       7,    0,   37,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QByteArray,    3,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,    5,    6,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void TcpServer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        TcpServer *_t = static_cast<TcpServer *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->pushSocketData((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        case 1: _t->sendStateMsg((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 2: _t->onServerNewConnection(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (TcpServer::*_t)(QByteArray );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TcpServer::pushSocketData)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (TcpServer::*_t)(QString , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TcpServer::sendStateMsg)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject TcpServer::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_TcpServer.data,
+      qt_meta_data_TcpServer,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *TcpServer::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TcpServer::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_TcpServer.stringdata0))
+        return static_cast<void*>(const_cast< TcpServer*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int TcpServer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void TcpServer::pushSocketData(QByteArray _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void TcpServer::sendStateMsg(QString _t1, int _t2)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+struct qt_meta_stringdata_TcpClient_t {
+    QByteArrayData data[12];
+    char stringdata0[139];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TcpClient_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TcpClient_t qt_meta_stringdata_TcpClient = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "TcpClient"
+QT_MOC_LITERAL(1, 10, 14), // "pushSocketData"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 5), // "bytes"
+QT_MOC_LITERAL(4, 32, 12), // "sendStateMsg"
+QT_MOC_LITERAL(5, 45, 3), // "msg"
+QT_MOC_LITERAL(6, 49, 4), // "flag"
+QT_MOC_LITERAL(7, 54, 16), // "onClientRecvData"
+QT_MOC_LITERAL(8, 71, 17), // "onReconnectServer"
+QT_MOC_LITERAL(9, 89, 21), // "onServerDisConnection"
+QT_MOC_LITERAL(10, 111, 11), // "onHeartBeat"
+QT_MOC_LITERAL(11, 123, 15) // "onConnectServer"
+
+    },
+    "TcpClient\0pushSocketData\0\0bytes\0"
+    "sendStateMsg\0msg\0flag\0onClientRecvData\0"
+    "onReconnectServer\0onServerDisConnection\0"
+    "onHeartBeat\0onConnectServer"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TcpClient[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   49,    2, 0x06 /* Public */,
+       4,    2,   52,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       7,    0,   57,    2, 0x08 /* Private */,
+       8,    0,   58,    2, 0x08 /* Private */,
+       9,    0,   59,    2, 0x08 /* Private */,
+      10,    0,   60,    2, 0x08 /* Private */,
+      11,    0,   61,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QByteArray,    3,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,    5,    6,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void TcpClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        TcpClient *_t = static_cast<TcpClient *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->pushSocketData((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        case 1: _t->sendStateMsg((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 2: _t->onClientRecvData(); break;
+        case 3: _t->onReconnectServer(); break;
+        case 4: _t->onServerDisConnection(); break;
+        case 5: _t->onHeartBeat(); break;
+        case 6: _t->onConnectServer(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (TcpClient::*_t)(QByteArray );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TcpClient::pushSocketData)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (TcpClient::*_t)(QString , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TcpClient::sendStateMsg)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject TcpClient::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_TcpClient.data,
+      qt_meta_data_TcpClient,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *TcpClient::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TcpClient::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_TcpClient.stringdata0))
+        return static_cast<void*>(const_cast< TcpClient*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int TcpClient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void TcpClient::pushSocketData(QByteArray _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void TcpClient::sendStateMsg(QString _t1, int _t2)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+struct qt_meta_stringdata_TcpClientSocket_t {
+    QByteArrayData data[6];
+    char stringdata0[77];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TcpClientSocket_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TcpClientSocket_t qt_meta_stringdata_TcpClientSocket = {
+    {
+QT_MOC_LITERAL(0, 0, 15), // "TcpClientSocket"
+QT_MOC_LITERAL(1, 16, 14), // "pushSocketData"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 5), // "bytes"
+QT_MOC_LITERAL(4, 38, 16), // "onServerReadData"
+QT_MOC_LITERAL(5, 55, 21) // "onServerDisConnection"
+
+    },
+    "TcpClientSocket\0pushSocketData\0\0bytes\0"
+    "onServerReadData\0onServerDisConnection"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TcpClientSocket[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   29,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    0,   32,    2, 0x08 /* Private */,
+       5,    0,   33,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QByteArray,    3,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void TcpClientSocket::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        TcpClientSocket *_t = static_cast<TcpClientSocket *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->pushSocketData((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        case 1: _t->onServerReadData(); break;
+        case 2: _t->onServerDisConnection(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (TcpClientSocket::*_t)(QByteArray );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&TcpClientSocket::pushSocketData)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject TcpClientSocket::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_TcpClientSocket.data,
+      qt_meta_data_TcpClientSocket,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *TcpClientSocket::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TcpClientSocket::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_TcpClientSocket.stringdata0))
+        return static_cast<void*>(const_cast< TcpClientSocket*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int TcpClientSocket::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void TcpClientSocket::pushSocketData(QByteArray _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_END_MOC_NAMESPACE
