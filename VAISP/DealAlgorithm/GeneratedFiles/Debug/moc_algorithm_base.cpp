da639560cc7b6010990c0fc6c97c856908626c70
yaoxw-njust <yaoxw13@163.com>
2021-03-03 15:42:50 +0800

    20210303太和最新代码提交

--- VAISP/DealAlgorithm/GeneratedFiles/Debug/moc_algorithm_base.cpp
+++ VAISP/DealAlgorithm/GeneratedFiles/Debug/moc_algorithm_base.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'algorithm_base.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'algorithm_base.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.7.1. It"
+#error "This file was generated using the moc from 5.5.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -92,28 +92,24 @@ void AlgorithmBase::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
             typedef void (AlgorithmBase::*_t)(QString , int );
             if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&AlgorithmBase::algorithmSendMsg)) {
                 *result = 0;
-                return;
             }
         }
         {
             typedef void (AlgorithmBase::*_t)();
             if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&AlgorithmBase::sendVibrationData)) {
                 *result = 1;
-                return;
             }
         }
         {
             typedef void (AlgorithmBase::*_t)();
             if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&AlgorithmBase::sendAxleData)) {
                 *result = 2;
-                return;
             }
         }
         {
             typedef void (AlgorithmBase::*_t)();
             if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&AlgorithmBase::sendElectricalData)) {
                 *result = 3;
-                return;
             }
         }
     }
